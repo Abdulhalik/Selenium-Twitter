c8febf9ae4a12f09365762158fdc399f6f6559eb
Abdulhalık Korkmaz <abdulhalik.korkmaz@stu.fsm.edu.tr>
2020-01-20 21:41:04 +0300

    Fetch Tweets from Twitter

--- searchByHashtags.py
+++ searchByHashtags.py
@@ -28,12 +28,44 @@ time.sleep(10)
 # Get Search Input Area First
 searchArea = browser.find_element_by_xpath("//*[@id='react-root']/div/div/div/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div[2]/input")
 # Send (set hashtag to search input area) any hashtag you specify
-searchArea.send_keys("#BigData")
+searchArea.send_keys("#SakirKocabas")
 time.sleep(3)
 
 # Press Enter after typed a hashtag to start searching
 searchArea.send_keys(Keys.ENTER)
-time.sleep(8)
+time.sleep(5)
+
+# To run the JavaScript in Selenium use execute.script()
+# To get all tweets browser must be scrolled, and this'll load another tweets so, to do this we have to run a simple script
+# -- Suana dek sadece yuklenen tweetleri alabiliyoruz. Daha fazla tweet icin browseri scroll edip yeni tweetler yuklemeliyiz.
+lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage = document.body.scrollHeight;return lenOfPage;")
+print(lenOfPage)
+match = False
+while match == False:
+    lastCount = lenOfPage
+    time.sleep(3)
+    # While scroll ends, do new scroll in every step
+    # -- Sayfanin sonuna gelene kadar surekli scroll et.
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage = document.body.scrollHeight;return lenOfPage;")
+    print(lenOfPage)
+    if lastCount == lenOfPage:
+        match = True
+
+time.sleep(5)
+
+# Tweet Text Selectors
+selectors = ".css-901oao.r-jwli3a.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0"
+tweetTexts = browser.find_elements_by_css_selector(selectors)
+
+# Write all tweets into the Txt File
+tweetCount = 1
+with open("tweets.txt", "w", encoding= "UTF-8") as file:
+    file.write("TWITTER - TWEETS ABOUT 'SAKIR KOCABAS' - #sakirkocabas \n")
+    file.write("\n------------------------------------------------------\n")
+    for tweet in tweetTexts:
+        file.write(str(tweetCount) + ".\n" + tweet.text + "\n")
+        file.write("*****************************************************\n")
+        tweetCount += 1
 
 # Navigating History in Browser use - driver.forward() / driver.back()
 # To return previous page (TimeLine)
